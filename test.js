4999646555ee8f32ae421e5cad6fc4ea3ea0a297
wxie2017 <xiewensheng@hotmail.com>
2020-06-09 21:38:32 +0800

    update exercise

--- test.js
+++ test.js
@@ -1,2 +1,13 @@
-a = "hello"
+const a = "hello"
 console.log(a)
+
+const solution = () => {
+    console.log(1)
+    console.log(2)
+    console.log(3)
+    return 4
+}
+
+//call
+const b = solution()
+console.log(b)
