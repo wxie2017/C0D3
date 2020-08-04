903e9ebc847adbb94dfe2eadd67e2b186e1240ad
wxie2017 <xiewensheng@hotmail.com>
2020-08-04 16:24:29 +0800

    update for JS2

---
+++ js2/preflight.js
@@ -0,0 +1,107 @@
+//js2 arrays
+//solution file
+
+const allFuns = {} // define the test object
+
+//Write a function named removeCharX that removes the character at the given index from a string.
+
+const removeCharX = (str, i, j=0, result="") => {
+    if ((i > 0) && (i < str.length)) {
+        if (j < str.length) {
+            if (j !== i) {
+              result = result + str[j]
+            }
+
+            return removeCharX(str, i, j+1, result)
+        }
+
+        return result
+    }
+
+    return str
+}
+
+//Write a function named less3Diff that determines whether two strings have fewer than 3 different characters. Both arguments will have the same length.
+const less3Diff = (str1, str2, i=0, count=0) => {
+    if (str1.length === str2.length) {
+        if (count < 3) {
+            if (i < str1.length) {
+                if (str1[i] !== str2[i]) {
+                    count = count + 1
+                }
+
+                return less3Diff(str1, str2, i+1, count)
+            }
+
+            return true
+        }
+
+        return false
+    }
+
+    return false
+}
+
+//Write a function named reverso that takes in a string, and returns a new string with the input string reversed.
+const reverso = (str, i=0, result="") => {
+    if (i < str.length) {
+        return reverso(str, i+1, str[i]+result)
+    }
+
+    return result
+}
+
+//Write a function named delayAndCall that takes in 2 parameters, a number and a function, and returns a function. When the returned function is called, the input function will execute after the input number of milliseconds.
+const delayAndCall = (n, fun) => {
+    return () => {
+        setTimeout(fun, n)
+    }
+}
+
+//Write a function named primeMachine that takes in a number and returns a function. Each time the returned function is called, return the next prime number (starting from the input number).
+const isPrime = (num, start=2) => {
+    if(num < start) {
+	return false
+    }
+    if(num === start) {
+	return true
+    }
+    if(num % start === 0) {
+	return false
+    }
+    return isPrime(num, start + 1)
+}
+
+const getNextPrime = (start) => {
+    if(isPrime(start)) {
+	return start
+    }
+    return getNextPrime(start + 1)
+}
+
+const primeMachine = (start) => {
+    if (start < 2) {
+        start = 1 // Any number smaller than 2 is not a prime.
+    }
+    if (isPrime(start)) {
+        start = start + 1
+    }
+    return () => {
+        const result = getNextPrime(start)
+        start = start + 1
+        return result
+    }
+}
+
+allFuns.removeCharX = removeCharX // You need this line for every function
+                                  // you write for test
+
+allFuns.less3Diff = less3Diff
+allFuns.reverso = reverso
+allFuns.delayAndCall = delayAndCall
+allFuns.primeMachine = primeMachine
+
+//call
+console.log(reverso("We're in the endgame now"))
+
+module.exports = allFuns // end of test
---
+++ js2/preflight.test.js
@@ -0,0 +1,110 @@
+// jest test file
+
+// test for removeCharX
+describe('removeCharX function', () => {
+    it('should remove the first character', () => {
+        const str = "We're in the endgame now."
+        const result = fn.removeCharX(str, 0)
+        expect(result).toEqual("e're in the endgame now.")
+    })
+    it('return the original string', () => {
+        const str = "a"
+        const result = fn.removeCharX(str, 3)
+        expect(result).toEqual("a")
+    })
+    it('return the original string', () => {
+        const str = "abc"
+        const result = fn.removeCharX(str, -3)
+        expect(result).toEqual("abc")
+    })
+})
+
+// test for less3Diff
+describe('less3Diff function', () => {
+    it('should return true because no character is different', () => {
+        const str1 = "spyderman"
+        const str2 = "spyderman"
+        const result = fn.less3Diff(str1, str2)
+        expect(result).toEqual(true)
+    })
+    it('should return true because only 1 character is different', () => {
+        const str1 = "spiderman"
+        const str2 = "spyderman"
+        const result = fn.less3Diff(str1, str2)
+        expect(result).toEqual(true)
+    })
+    it('should return true because 2 characters are different', () => {
+        const str1 = "spiderman"
+        const str2 = "spydermam"
+        const result = fn.less3Diff(str1, str2)
+        expect(result).toEqual(true)
+    })
+    it('should return false because 3 characters are different', () => {
+        const str1 = "spiderman"
+        const str2 = "spydormam"
+        const result = fn.less3Diff(str1, str2)
+        expect(result).toEqual(false)
+    })
+    it('should return false because 4 characters are different', () => {
+        const str1 = "zpidormam"
+        const str2 = "spyderman"
+        const result = fn.less3Diff(str1, str2)
+        expect(result).toEqual(false)
+    })
+})
+
+// test for reverso
+describe('reverso function', () => {
+    it('should return reversed empty string', () => {
+        const str = ""
+        const result = fn.reverso(str)
+        expect(result).toEqual("")
+    })
+    it('should return reversed 1 character string', () => {
+        const str = "s"
+        const result = fn.reverso(str)
+        expect(result).toEqual("s")
+    })
+    it('should return reversed 2 characters string', () => {
+        const str = "sa"
+        const result = fn.reverso(str)
+        expect(result).toEqual("as")
+    })
+    it('should return reversed 3 characters string', () => {
+        const str = "sab"
+        const result = fn.reverso(str)
+        expect(result).toEqual("bas")
+    })
+})
+
+// test for delayAndCall
+describe('delayAndCall function', () => {
+    it('should return hello', () => {
+        const n = 1000
+        const fun = () => {
+            console.log('I am groot')
+        }
+        const result = fn.delayAndCall(n, fun)
+        expect(result).toEqual("I am groot")
+    })
+})
+
+// test for primeMachine
+describe('primeMachine function', () => {
+    it('should return next 2 primes starting with a negative number', () => {
+        const getPrime1 = fn.primeMachine(-2)
+        expect(getPrime1()).toEqual(2)
+        expect(getPrime1()).toEqual(3)
+    })
+    it('should not return starting number if prime', () => {
+        const getPrime2 = fn.primeMachine(5)
+        expect(getPrime2()).toEqual(7)
+        expect(getPrime2()).toEqual(11)
+    })
+    it('should return next 3 primes starting at 10', () => {
+        const getPrime3 = fn.primeMachine(10)
+        expect(getPrime3()).toEqual(11)
+        expect(getPrime3()).toEqual(13)
+        expect(getPrime3()).toEqual(17)
+    })
+})
